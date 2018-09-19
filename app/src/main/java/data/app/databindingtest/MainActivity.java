9951fd56dd6970d7d0ad962f77e0d35592459970
yueunyeol <eunyull102@naver.com>
2018-09-19 18:01:23 +0900

    [add] AbstractAccountAuthenticator 추가

--- app/src/main/java/data/app/databindingtest/MainActivity.java
+++ app/src/main/java/data/app/databindingtest/MainActivity.java
@@ -3,7 +3,7 @@ package data.app.databindingtest;
 import android.databinding.DataBindingUtil;
 import android.databinding.ObservableArrayList;
 import android.os.Bundle;
-import android.databinding.DataBindingUtil;
+import android.accounts.AbstractAccountAuthenticator;
 import android.support.v7.app.AppCompatActivity;
 
 
